1985bb3a4de53d3c17cf63d13c8d2fc70681b5a7
abdul.rauf <abdul.rauf@riksof.com>
2015-05-30 12:46:26 +0500

    initial commit, xib base swift project

---
+++ rufiSwift/Classes/Dashboard.swift
@@ -0,0 +1,24 @@
+//
+//  Dashboard.swift
+//  Chimani
+//
+//  Created by Abdul Rauf on 27/05/2015.
+//  Copyright (c) 2015 RIKSOF. All rights reserved.
+//
+
+import UIKit
+
+class Dashboard: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "Dashboard"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
